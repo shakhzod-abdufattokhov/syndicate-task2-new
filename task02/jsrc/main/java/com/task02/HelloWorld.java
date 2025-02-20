2ebf735180df2a3061cc61e8ec811695bf480ce2
Abdufattoxov <abdufattoxovshaxzod0404@gmail.com>
2025-02-20 17:45:19 +0500

    initial commit

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -29,7 +29,7 @@ public class HelloWorld implements RequestHandler<Map<String, Object>, Map<Strin
 	@Override
 	public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
 		String path = (String) input.get("path");
-		String method = (String) input.get("httpMethod");
+		String method = (String) input.get("method");
 
 		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
 			return createResponse(200, "Hello from Lambda");
