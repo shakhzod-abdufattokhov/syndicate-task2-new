6156a0f467814ff8ab2790b55aefdbab991446a8
Abdufattoxov <abdufattoxovshaxzod0404@gmail.com>
2025-03-02 13:49:56 +0500

    task3

--- task03/jsrc/main/java/com/task03/HelloWord.java
+++ task03/jsrc/main/java/com/task03/HelloWord.java
@@ -2,8 +2,6 @@ package com.task03;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -17,25 +15,13 @@ import java.util.Map;
 		aliasName = "${lambdas_alias_name}",
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
-public class HelloWord implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-
-	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-		context.getLogger().log("Received request: " + request.getPath());
-
-		if (!"/hello".equals(request.getPath())) {
-			return new APIGatewayProxyResponseEvent()
-					.withStatusCode(404)
-					.withBody("{\"error\": \"Not Found\"}");
-		}
-
-		Map<String, String> responseBody = new HashMap<>();
-		responseBody.put("message", "Hello from Lambda");
-
-		return new APIGatewayProxyResponseEvent()
-				.withStatusCode(200)
-				.withHeaders(Map.of("Content-Type", "application/json"))
-				.withBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
+		return resultMap;
 	}
 }
