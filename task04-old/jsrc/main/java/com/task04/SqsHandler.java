2ab7dda430b6bef91188729fa84305a84f84dbf1
Abdufattoxov <abdufattoxovshaxzod0404@gmail.com>
2025-03-03 17:56:26 +0500

    task4

---
+++ task04-old/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,54 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import net.sf.aws.syndicate.deployment.annotations.lambda.LambdaHandler;
+import net.sf.aws.syndicate.deployment.annotations.events.SqsTrigger;
+import net.sf.aws.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(
+		lambdaName = "sqs_handler",
+		roleName = "sqs_handler-role",
+		isPublishVersion = true,
+		aliasName = "${lambdas_alias_name}",
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SqsTrigger(
+		queueArn = "arn:aws:sqs:eu-central-1:905418349556:cmtr-2028f2b4-async_queue"
+)
+public class SqsHandler implements RequestHandler<SQSEvent, Map<String, Object>> {
+
+	@Override
+	public Map<String, Object> handleRequest(SQSEvent event, Context context) {
+		context.getLogger().log("Lambda triggered by SQS message. Processing messages...\n");
+
+		Map<String, Object> resultMap = new HashMap<>();
+		if (event.getRecords().isEmpty()) {
+			context.getLogger().log("No messages received from SQS.\n");
+			resultMap.put("statusCode", 204);
+			resultMap.put("message", "No messages received");
+			return resultMap;
+		}
+
+		for (SQSEvent.SQSMessage message : event.getRecords()) {
+			try {
+				context.getLogger().log(String.format(
+						"Message ID: %s | Body: %s | Attributes: %s\n",
+						message.getMessageId(),
+						message.getBody(),
+						message.getAttributes()
+				));
+			} catch (Exception e) {
+				context.getLogger().log("Error processing message: " + e.getMessage() + "\n");
+			}
+		}
+
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "SQS Messages processed successfully");
+		return resultMap;
+	}
+}
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -2,9 +2,8 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.events.SqsEventSource;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
@@ -17,38 +16,14 @@ import java.util.Map;
 		aliasName = "${lambdas_alias_name}",
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@SqsEventSource(
-		queueArn = "arn:aws:sqs:eu-central-1:905418349556:cmtr-2028f2b4-async_queue"
-)
-public class SqsHandler implements RequestHandler<SQSEvent, Map<String, Object>> {
+@SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 123)
+public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
 
-	@Override
-	public Map<String, Object> handleRequest(SQSEvent event, Context context) {
-		context.getLogger().log("Lambda triggered by SQS message. Processing messages...\n");
-
-		Map<String, Object> resultMap = new HashMap<>();
-		if (event.getRecords().isEmpty()) {
-			context.getLogger().log("No messages received from SQS.\n");
-			resultMap.put("statusCode", 204);
-			resultMap.put("message", "No messages received");
-			return resultMap;
-		}
-
-		for (SQSEvent.SQSMessage message : event.getRecords()) {
-			try {
-				context.getLogger().log(String.format(
-						"Message ID: %s | Body: %s | Attributes: %s\n",
-						message.getMessageId(),
-						message.getBody(),
-						message.getAttributes()
-				));
-			} catch (Exception e) {
-				context.getLogger().log("Error processing message: " + e.getMessage() + "\n");
-			}
-		}
-
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("statusCode", 200);
-		resultMap.put("message", "SQS Messages processed successfully");
+		resultMap.put("message", "Hello from Lambda");
 		return resultMap;
 	}
 }
